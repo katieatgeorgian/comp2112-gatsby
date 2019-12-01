ae6e5e71f995578c7bd8fe3df12f0584c1d7d471
katieatgeorgian <47111708+katieatgeorgian@users.noreply.github.com>
2019-11-30 20:25:00 -0500

    change picture

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "applecaramelcupcake.jpeg" }) {
+      placeholderImage: file(relativePath: { eq: "christie.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
